7637d723b0eb48aca394a4c78268a1cd24e2c778
ricod <26433@ma-web.nl>
2018-11-08 13:30:38 +0100

    Mijn ding

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("Hey daar wij hebben broeken in de aanbieden zou je er een willen??");
         Scanner s = new Scanner(System.in);
         String antwoord;
+        int geld = 20;
 
         antwoord = s.next();
         if (antwoord.equals("Ja")) {
@@ -14,41 +15,115 @@ public class Main {
         } else if (antwoord.equals("nee")) {
             System.out.println("Oke sorry dan hebben wij hier niks voor jou!");
         }
-        String dure = s.next();
-        String goedkope = s.next();
-        if (dure.equals("dure")) {
+        String type = s.next();
+        if (type.equals("dure")) {
             System.out.println("Stone island of armani?");
-        } else if (goedkope.equals("goedkope")) {
+        } else if (type.equals("goedkope")) {
             System.out.println("Cars of vingino?");
         }
-        String cars = s.next();
-        String vingino = s.next();
-        if (cars.equals("cars")) {
-            System.out.println("Ok, komt goed!");
-        }else if (vingino.equals("vingino"))
+        String merk = s.next();
+        if (merk.equals("cars")) {
+            geld = geld - 10;
+            System.out.println("Dat kost dan €10");
+            System.out.println(geld);
+        }else if (merk.equals("vingino"))
         {
-            System.out.println("Komt eraan!");
+            geld = geld - 10;
+            System.out.println("Dat kost dan €10");
+            System.out.println(geld);
         }
         String top = s.next();
-        if(top.equals("top"))
+        if(top.equals("alsjeblieft"))
         {
-            System.out.println("Graag gedaan!");
+            System.out.println("" +
+                    ",==c==.\n" +
+                    "|_/|\\_|\n" +
+                    "| ´|` |\n" +
+                    "|  |  |\n" +
+                    "|  |  |\n" +
+                    "|__|__|" +
+                    "zou je dan ook nog koffie willen? Dat kost dan €2");
+        }
+        String answer = s.next();
+        if (answer.equals("ja"))
+        {
+            geld = geld - 2;
+            System.out.println("         {\n" +
+                    "      {   }\n" +
+                    "       }_{ __{\n" +
+                    "    .-{   }   }-.\n" +
+                    "   (   }     {   )\n" +
+                    "   |`-.._____..-'|\n" +
+                    "   |             ;--.\n" +
+                    "   |            (__  \\\n" +
+                    "   |             | )  )\n" +
+                    "   |             |/  /\n" +
+                    "   |             /  /\n" +
+                    "   |            (  /\n" +
+                    "   \\             y'\n" +
+                    "    `-.._____..-'\n");
+            System.out.println(geld);
+
+        }
+        else if (answer.equals("nee"))
+        {
+            System.out.println("Fijne dag verder!");
         }
 
-        String stone = s.next();
-        String armani = s.next();
-        if(stone.equals("stone"))
+        String duur = s.next();
+        if(duur.equals("stone"))
         {
-            System.out.println("Komt eraan!");
+            geld = geld - 10;
+            System.out.println("Oke dat kost dan €15");
+            System.out.println(geld);
         }
-        else if(armani.equals("armani"))
+        else if(duur.equals("armani"))
         {
-           System.out.println("Broek is onderweg!");
+            geld = geld - 15;
+           System.out.println("Oke dat kost dan €15");
+            System.out.println(geld);
         }
         String bedankt = s.next();
-        if (bedankt.equals("bedankt"))
-        {
-            System.out.println("Graag gedaan!");
+        if (bedankt.equals("alsjeblieft")) {
+            System.out.println("     _____________\n" +
+                    "               [_I_I[L]=_I_I_]\n" +
+                    "               /     | :     \\\n" +
+                    "               |    /|  \\    |\n" +
+                    "               |   | '-  |   |\n" +
+                    "               \\  | /^\\ |  /\n" +
+                    "                |  | | | |  |\n" +
+                    "                |  | | | |  |\n" +
+                    "                |  | | | |  |\n" +
+                    "               \\  | | | |  /\n" +
+                    "                / -|-| |-|- \\\n" +
+                    "                |  | | | |  |\n" +
+                    "                |  | | | |  |\n" +
+                    "                |  | | | |  |\n" +
+                    "                |__|_| |_|__|\n" +
+                    "                [____] [____]" +
+                    "Zou je er nog koffie bij willen?");
+            String janee = s.next();
+            if (janee.equals("Ja")) {
+                geld = geld - 2;
+                System.out.println("         {\n" +
+                        "      {   }\n" +
+                        "       }_{ __{\n" +
+                        "    .-{   }   }-.\n" +
+                        "   (   }     {   )\n" +
+                        "   |`-.._____..-'|\n" +
+                        "   |             ;--.\n" +
+                        "   |            (__  \\\n" +
+                        "   |             | )  )\n" +
+                        "   |             |/  /\n" +
+                        "   |             /  /\n" +
+                        "   |            (  /\n" +
+                        "   \\             y'\n" +
+                        "    `-.._____..-'\n");
+                System.out.println(geld);
+            } else if (janee.equals("nee"))
+            {
+                System.out.println("Fijne dag verder!");
+            }
         }
 
     }
